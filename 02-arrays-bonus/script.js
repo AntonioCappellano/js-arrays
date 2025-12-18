9dc2a41132323f75bf1059b021e2538d246bd243
AntonioCappellano <antoniocappellano97@gmail.com>
2025-12-18 16:56:21 +0100

    just characters >= 5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -13,7 +13,15 @@ console.table(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+let longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames[longNames.length] = teachers[i];
+  }
+}
+
+console.table(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
