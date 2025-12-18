e48da218a6f7f8825914079e05aca5e55582f43c
AntonioCappellano <antoniocappellano97@gmail.com>
2025-12-18 14:31:00 +0100

    correct link index

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,12 +1,7 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"];
+
+console.table(teachers);
+// NON MODIFICARE QUESTA VARIABILE
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
