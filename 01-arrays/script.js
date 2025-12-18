ae3c97a071e0b76fe5f873cf5ec203e23ca2e398
AntonioCappellano <antoniocappellano97@gmail.com>
2025-12-18 15:23:47 +0100

    verify array if empty

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -35,4 +35,5 @@ const fourthTeacher = null;
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty);
